35f96f9a1bf70a063856be7481652dc7b53ee3ca
oOWenderOo <gabriel.moczalla@st.ovgu.de>
2015-07-19 16:48:46 +0200

    - Levelauswahl hinzugefügt

---
+++ GameProject/Crack_Tomb/Crack_Tomb/Crack_Tomb/Menuestruktur/LevelButton.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Crack_Tomb.Menuestruktur
+{
+    class LevelButton
+    {
+        Vector2 position;
+        Texture2D texture;
+        string text;
+        SpriteFont font;
+        int levelnummer;
+
+        public LevelButton(int levelnummer, Vector2 position, string text)
+        {
+            this.levelnummer = levelnummer;
+            this.position = position;
+            this.text = text;
+        }
+
+        public void SetFont(SpriteFont font)
+        {
+            this.font = font;
+        }
+
+        public void SetTexture(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+
+        public bool isPressed()
+        {
+            var mousestate = Mouse.GetState();
+            Vector2 mouseposition = new Vector2(mousestate.X, mousestate.Y);
+
+            int hoehe = texture.Height;
+            int breite = texture.Width;
+
+            if (mousestate.LeftButton == ButtonState.Pressed && mouseposition.X > position.X && mouseposition.Y > position.Y && mouseposition.Y < (position.Y + hoehe) && mouseposition.X < (position.X + breite))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int getLevelnummer()
+        {
+            return levelnummer;
+        }
+
+        public void Draw(GameTime gameTime, GraphicsDeviceManager Graphics, SpriteBatch SpriteBatch, int levelnummer)
+        {
+            if (this.levelnummer != levelnummer)
+            {
+                SpriteBatch.Draw(texture, position, Color.White);
+                SpriteBatch.DrawString(font, text, new Vector2(position.X + 20, position.Y + 15), Color.Black);
+            }
+            else
+            {
+                SpriteBatch.Draw(texture, position, Color.Red);
+                SpriteBatch.DrawString(font, text, new Vector2(position.X + 20, position.Y + 15), Color.White);
+            }
+        }
+    }
+}
--- GameProject/Crack_Tomb/Crack_Tomb/Crack_Tomb/Menuestruktur/Levelauswahl.cs
+++ GameProject/Crack_Tomb/Crack_Tomb/Crack_Tomb/Menuestruktur/Levelauswahl.cs
@@ -6,6 +6,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Input;
+using Crack_Tomb.Menuestruktur;
 
 namespace MainMenuCo
 {
@@ -14,16 +15,28 @@ namespace MainMenuCo
         SpriteFont fontButton;
         SpriteFont fontText;
         Button[] buttons = new Button[2];
+        LevelButton[] levelbuttons;
         Texture2D mouse;
         Texture2D background;
 
         int wartezeit;
         int levelnummer = 2;
+        int anzahl_level = 3;
 
         public Levelauswahl()
         {
             buttons[0] = new Button(new Vector2(60, 370), "MainMenu", "Zurück");
             buttons[1] = new Button(new Vector2(540, 370), "InGame", "Level starten");
+
+            levelbuttons = new LevelButton[anzahl_level];
+
+            for (int i = 0; i < anzahl_level; i++)
+            {
+                string text = "";
+                text = text + (i + 1);
+                levelbuttons[i] = new LevelButton(i + 1, new Vector2(300, 100 + 50 * i + i), text);
+                text = "";
+            }
 
             wartezeit = 6;
         }
@@ -38,6 +51,13 @@ namespace MainMenuCo
                 buttons[i].SetTexture(content.Load<Texture2D>("button"));
                 buttons[i].SetFont(fontButton);
             }
+
+            for (int i = 0; i < anzahl_level; i++)
+            {
+                levelbuttons[i].SetTexture(content.Load<Texture2D>("levelbutton"));
+                levelbuttons[i].SetFont(fontButton);
+            }
+
             mouse = content.Load<Texture2D>("MouseZeiger");
             background = content.Load<Texture2D>("Testbildhintergrund");
         }
@@ -55,6 +75,13 @@ namespace MainMenuCo
                 if (buttons[i].isPressed())
                     return buttons[i].GetState(levelnummer);
             }
+
+            for (int i = 0; i < anzahl_level; i++)
+            {
+                if (levelbuttons[i].isPressed())
+                    levelnummer = levelbuttons[i].getLevelnummer();
+            }
+
             return this;
         }
 
@@ -64,10 +91,14 @@ namespace MainMenuCo
             SpriteBatch.Draw(background, new Vector2(0, 0), Color.White);
             SpriteBatch.DrawString(fontText, "Levelauswahl", new Vector2(60, 50), Color.Black);
 
-
             for (int i = 0; i < buttons.Length; i++)
             {
                 buttons[i].Draw(gameTime, Graphics, SpriteBatch);
+            }
+
+            for (int i = 0; i < anzahl_level; i++)
+            {
+                levelbuttons[i].Draw(gameTime, Graphics, SpriteBatch, levelnummer);
             }
 
             SpriteBatch.Draw(mouse, new Vector2(Mouse.GetState().X, Mouse.GetState().Y), Color.White);
