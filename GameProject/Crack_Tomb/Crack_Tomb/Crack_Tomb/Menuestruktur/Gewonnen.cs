e2adbfa4bd204bd2554278c5e425e77f09def9ca
oOWenderOo <gabriel.moczalla@st.ovgu.de>
2015-08-13 16:54:26 +0200

    - man kann sich nun in die Rangliste eintragen(alles bis auf Sonderzeichen)

--- GameProject/Crack_Tomb/Crack_Tomb/Crack_Tomb/Menuestruktur/Gewonnen.cs
+++ GameProject/Crack_Tomb/Crack_Tomb/Crack_Tomb/Menuestruktur/Gewonnen.cs
@@ -26,6 +26,39 @@ namespace Crack_Tomb.Menuestruktur
         bool musseintragen = false;
         string EingabeName = "Hallo";
 
+        bool isGedrücktA = false,
+             isGedrücktÄ = false,
+             isGedrücktB = false,
+             isGedrücktC = false,
+             isGedrücktD = false,
+             isGedrücktE = false,
+             isGedrücktF = false,
+             isGedrücktG = false,
+             isGedrücktH = false,
+             isGedrücktI = false,
+             isGedrücktJ = false,
+             isGedrücktK = false,
+             isGedrücktL = false,
+             isGedrücktM = false,
+             isGedrücktN = false,
+             isGedrücktO = false,
+             isGedrücktÖ = false,
+             isGedrücktP = false,
+             isGedrücktQ = false,
+             isGedrücktR = false,
+             isGedrücktS = false,
+             isGedrücktT = false,
+             isGedrücktU = false,
+             isGedrücktÜ = false,
+             isGedrücktV = false,
+             isGedrücktW = false,
+             isGedrücktX = false,
+             isGedrücktY = false,
+             isGedrücktZ = false,
+             isGedrücktUmschalt = false,
+             isGedrücktBack = false,
+             isGedrücktSpace = false;
+
         public Gewonnen(int levelnummer, int punkte)
         {
             this.levelnummer = levelnummer;
@@ -127,6 +160,22 @@ namespace Crack_Tomb.Menuestruktur
         {
             if (musseintragen)
             {
+                if (isGedrücktBack == false && Keyboard.GetState().IsKeyDown(Keys.Back) && EingabeName.Length > 0)
+                {
+                    isGedrücktBack = true;
+                }
+
+                if (EingabeName.Length <= 25)
+                {
+                    eingabeKontrolle();
+                }
+
+                if (isGedrücktBack == true && Keyboard.GetState().IsKeyUp(Keys.Back))
+                {
+                    isGedrücktBack = false;
+                    EingabeName = EingabeName.Substring(0, EingabeName.Length - 1);
+                }
+
                 if (Keyboard.GetState().IsKeyDown(Keys.Enter) && EingabeName.Length > 0)
                 {
                     string dateiname = "Level" + levelnummer + ".txt";
@@ -200,5 +249,530 @@ namespace Crack_Tomb.Menuestruktur
             SpriteBatch.End();
         }
 
+        private void eingabeKontrolle()
+        {
+            //Was passiert, wenn eine Taste gedrückt wird?
+            if (isGedrücktA == false && Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                isGedrücktA = true;
+            }
+
+            if (isGedrücktÄ == false && Keyboard.GetState().IsKeyDown(Keys.OemQuotes))
+            {
+                isGedrücktÄ = true;
+            }
+
+            if (isGedrücktB == false && Keyboard.GetState().IsKeyDown(Keys.B))
+            {
+                isGedrücktB = true;
+            }
+
+            if (isGedrücktC == false && Keyboard.GetState().IsKeyDown(Keys.C))
+            {
+                isGedrücktC = true;
+            }
+
+            if (isGedrücktD == false && Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                isGedrücktD = true;
+            }
+
+            if (isGedrücktE == false && Keyboard.GetState().IsKeyDown(Keys.E))
+            {
+                isGedrücktE = true;
+            }
+
+            if (isGedrücktF == false && Keyboard.GetState().IsKeyDown(Keys.F))
+            {
+                isGedrücktF = true;
+            }
+
+            if (isGedrücktG == false && Keyboard.GetState().IsKeyDown(Keys.G))
+            {
+                isGedrücktG = true;
+            }
+
+            if (isGedrücktH == false && Keyboard.GetState().IsKeyDown(Keys.H))
+            {
+                isGedrücktH = true;
+            }
+
+            if (isGedrücktI == false && Keyboard.GetState().IsKeyDown(Keys.I))
+            {
+                isGedrücktI = true;
+            }
+
+            if (isGedrücktJ == false && Keyboard.GetState().IsKeyDown(Keys.J))
+            {
+                isGedrücktJ = true;
+            }
+
+            if (isGedrücktK == false && Keyboard.GetState().IsKeyDown(Keys.K))
+            {
+                isGedrücktK = true;
+            }
+
+            if (isGedrücktL == false && Keyboard.GetState().IsKeyDown(Keys.L))
+            {
+                isGedrücktL = true;
+            }
+
+            if (isGedrücktM == false && Keyboard.GetState().IsKeyDown(Keys.M))
+            {
+                isGedrücktM = true;
+            }
+
+            if (isGedrücktN == false && Keyboard.GetState().IsKeyDown(Keys.N))
+            {
+                isGedrücktN = true;
+            }
+
+            if (isGedrücktO == false && Keyboard.GetState().IsKeyDown(Keys.O))
+            {
+                isGedrücktO = true;
+            }
+
+            if (isGedrücktÖ == false && Keyboard.GetState().IsKeyDown(Keys.OemTilde))
+            {
+                isGedrücktÖ = true;
+            }
+
+            if (isGedrücktP == false && Keyboard.GetState().IsKeyDown(Keys.P))
+            {
+                isGedrücktP = true;
+            }
+
+            if (isGedrücktQ == false && Keyboard.GetState().IsKeyDown(Keys.Q))
+            {
+                isGedrücktQ = true;
+            }
+
+            if (isGedrücktR == false && Keyboard.GetState().IsKeyDown(Keys.R))
+            {
+                isGedrücktR = true;
+            }
+
+            if (isGedrücktS == false && Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                isGedrücktS = true;
+            }
+
+            if (isGedrücktT == false && Keyboard.GetState().IsKeyDown(Keys.T))
+            {
+                isGedrücktT = true;
+            }
+
+            if (isGedrücktU == false && Keyboard.GetState().IsKeyDown(Keys.U))
+            {
+                isGedrücktU = true;
+            }
+
+            if (isGedrücktÜ == false && Keyboard.GetState().IsKeyDown(Keys.OemSemicolon))
+            {
+                isGedrücktÜ = true;
+            }
+
+            if (isGedrücktV == false && Keyboard.GetState().IsKeyDown(Keys.V))
+            {
+                isGedrücktV = true;
+            }
+
+            if (isGedrücktW == false && Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                isGedrücktW = true;
+            }
+
+            if (isGedrücktX == false && Keyboard.GetState().IsKeyDown(Keys.X))
+            {
+                isGedrücktX = true;
+            }
+
+            if (isGedrücktY == false && Keyboard.GetState().IsKeyDown(Keys.Y))
+            {
+                isGedrücktY = true;
+            }
+
+            if (isGedrücktZ == false && Keyboard.GetState().IsKeyDown(Keys.Z))
+            {
+                isGedrücktZ = true;
+            }
+
+            if (isGedrücktUmschalt == false && (Keyboard.GetState().IsKeyDown(Keys.LeftShift) || Keyboard.GetState().IsKeyDown(Keys.RightShift)))
+            {
+                isGedrücktUmschalt = true;
+            }
+
+            if (isGedrücktSpace == false && Keyboard.GetState().IsKeyDown(Keys.Space) && EingabeName.Length > 0)
+            {
+                isGedrücktSpace = true;
+            }
+
+            //Was passiert, wenn eine Taste losgelassen wird?
+            if (isGedrücktSpace == true && Keyboard.GetState().IsKeyUp(Keys.Space))
+            {
+                isGedrücktSpace = false;
+                EingabeName += " ";
+            }
+
+            if (isGedrücktUmschalt == false)
+            {
+                //Kleinbuchstaben
+                if (isGedrücktA == true && Keyboard.GetState().IsKeyUp(Keys.A))
+                {
+                    isGedrücktA = false;
+                    EingabeName += "a";
+                }
+
+                if (isGedrücktÄ == true && Keyboard.GetState().IsKeyUp(Keys.OemQuotes))
+                {
+                    isGedrücktÄ = false;
+                    EingabeName += "ä";
+                }
+
+                if (isGedrücktB == true && Keyboard.GetState().IsKeyUp(Keys.B))
+                {
+                    isGedrücktB = false;
+                    EingabeName += "b";
+                }
+
+                if (isGedrücktC == true && Keyboard.GetState().IsKeyUp(Keys.C))
+                {
+                    isGedrücktC = false;
+                    EingabeName += "c";
+                }
+
+                if (isGedrücktD == true && Keyboard.GetState().IsKeyUp(Keys.D))
+                {
+                    isGedrücktD = false;
+                    EingabeName += "d";
+                }
+
+                if (isGedrücktE == true && Keyboard.GetState().IsKeyUp(Keys.E))
+                {
+                    isGedrücktE = false;
+                    EingabeName += "e";
+                }
+
+                if (isGedrücktF == true && Keyboard.GetState().IsKeyUp(Keys.F))
+                {
+                    isGedrücktF = false;
+                    EingabeName += "f";
+                }
+
+                if (isGedrücktG == true && Keyboard.GetState().IsKeyUp(Keys.G))
+                {
+                    isGedrücktG = false;
+                    EingabeName += "g";
+                }
+
+                if (isGedrücktH == true && Keyboard.GetState().IsKeyUp(Keys.H))
+                {
+                    isGedrücktH = false;
+                    EingabeName += "h";
+                }
+
+                if (isGedrücktI == true && Keyboard.GetState().IsKeyUp(Keys.I))
+                {
+                    isGedrücktI = false;
+                    EingabeName += "i";
+                }
+
+                if (isGedrücktJ == true && Keyboard.GetState().IsKeyUp(Keys.J))
+                {
+                    isGedrücktJ = false;
+                    EingabeName += "j";
+                }
+
+                if (isGedrücktK == true && Keyboard.GetState().IsKeyUp(Keys.K))
+                {
+                    isGedrücktK = false;
+                    EingabeName += "k";
+                }
+
+                if (isGedrücktL == true && Keyboard.GetState().IsKeyUp(Keys.L))
+                {
+                    isGedrücktL = false;
+                    EingabeName += "l";
+                }
+
+                if (isGedrücktM == true && Keyboard.GetState().IsKeyUp(Keys.M))
+                {
+                    isGedrücktM = false;
+                    EingabeName += "m";
+                }
+
+                if (isGedrücktN == true && Keyboard.GetState().IsKeyUp(Keys.N))
+                {
+                    isGedrücktN = false;
+                    EingabeName += "n";
+                }
+
+                if (isGedrücktO == true && Keyboard.GetState().IsKeyUp(Keys.O))
+                {
+                    isGedrücktO = false;
+                    EingabeName += "o";
+                }
+
+                if (isGedrücktÖ == true && Keyboard.GetState().IsKeyUp(Keys.OemTilde))
+                {
+                    isGedrücktÖ = false;
+                    EingabeName += "ö";
+                }
+
+                if (isGedrücktP == true && Keyboard.GetState().IsKeyUp(Keys.P))
+                {
+                    isGedrücktP = false;
+                    EingabeName += "p";
+                }
+
+                if (isGedrücktQ == true && Keyboard.GetState().IsKeyUp(Keys.Q))
+                {
+                    isGedrücktQ = false;
+                    EingabeName += "q";
+                }
+
+                if (isGedrücktR == true && Keyboard.GetState().IsKeyUp(Keys.R))
+                {
+                    isGedrücktR = false;
+                    EingabeName += "r";
+                }
+
+                if (isGedrücktS == true && Keyboard.GetState().IsKeyUp(Keys.S))
+                {
+                    isGedrücktS = false;
+                    EingabeName += "s";
+                }
+
+                if (isGedrücktT == true && Keyboard.GetState().IsKeyUp(Keys.T))
+                {
+                    isGedrücktT = false;
+                    EingabeName += "t";
+                }
+
+                if (isGedrücktU == true && Keyboard.GetState().IsKeyUp(Keys.U))
+                {
+                    isGedrücktU = false;
+                    EingabeName += "u";
+                }
+
+                if (isGedrücktÜ == true && Keyboard.GetState().IsKeyUp(Keys.OemSemicolon))
+                {
+                    isGedrücktÜ = false;
+                    EingabeName += "ü";
+                }
+
+                if (isGedrücktV == true && Keyboard.GetState().IsKeyUp(Keys.V))
+                {
+                    isGedrücktV = false;
+                    EingabeName += "v";
+                }
+
+                if (isGedrücktW == true && Keyboard.GetState().IsKeyUp(Keys.W))
+                {
+                    isGedrücktW = false;
+                    EingabeName += "w";
+                }
+
+                if (isGedrücktX == true && Keyboard.GetState().IsKeyUp(Keys.X))
+                {
+                    isGedrücktX = false;
+                    EingabeName += "x";
+                }
+
+                if (isGedrücktY == true && Keyboard.GetState().IsKeyUp(Keys.Y))
+                {
+                    isGedrücktY = false;
+                    EingabeName += "y";
+                }
+
+                if (isGedrücktZ == true && Keyboard.GetState().IsKeyUp(Keys.Z))
+                {
+                    isGedrücktZ = false;
+                    EingabeName += "z";
+                }
+            }
+            else
+            {
+                if (Keyboard.GetState().IsKeyUp(Keys.LeftShift) || Keyboard.GetState().IsKeyUp(Keys.RightShift))
+                {
+                    isGedrücktUmschalt = false;
+                }
+
+                //Großbuchstaben
+                if (isGedrücktA == true && Keyboard.GetState().IsKeyUp(Keys.A))
+                {
+                    isGedrücktA = false;
+                    EingabeName += "A";
+                }
+
+                if (isGedrücktÄ == true && Keyboard.GetState().IsKeyUp(Keys.OemQuotes))
+                {
+                    isGedrücktÄ = false;
+                    EingabeName += "Ä";
+                }
+
+                if (isGedrücktB == true && Keyboard.GetState().IsKeyUp(Keys.B))
+                {
+                    isGedrücktB = false;
+                    EingabeName += "B";
+                }
+
+                if (isGedrücktC == true && Keyboard.GetState().IsKeyUp(Keys.C))
+                {
+                    isGedrücktC = false;
+                    EingabeName += "C";
+                }
+
+                if (isGedrücktD == true && Keyboard.GetState().IsKeyUp(Keys.D))
+                {
+                    isGedrücktD = false;
+                    EingabeName += "D";
+                }
+
+                if (isGedrücktE == true && Keyboard.GetState().IsKeyUp(Keys.E))
+                {
+                    isGedrücktE = false;
+                    EingabeName += "E";
+                }
+
+                if (isGedrücktF == true && Keyboard.GetState().IsKeyUp(Keys.F))
+                {
+                    isGedrücktF = false;
+                    EingabeName += "F";
+                }
+
+                if (isGedrücktG == true && Keyboard.GetState().IsKeyUp(Keys.G))
+                {
+                    isGedrücktG = false;
+                    EingabeName += "G";
+                }
+
+                if (isGedrücktH == true && Keyboard.GetState().IsKeyUp(Keys.H))
+                {
+                    isGedrücktH = false;
+                    EingabeName += "H";
+                }
+
+                if (isGedrücktI == true && Keyboard.GetState().IsKeyUp(Keys.I))
+                {
+                    isGedrücktI = false;
+                    EingabeName += "I";
+                }
+
+                if (isGedrücktJ == true && Keyboard.GetState().IsKeyUp(Keys.J))
+                {
+                    isGedrücktJ = false;
+                    EingabeName += "J";
+                }
+
+                if (isGedrücktK == true && Keyboard.GetState().IsKeyUp(Keys.K))
+                {
+                    isGedrücktK = false;
+                    EingabeName += "K";
+                }
+
+                if (isGedrücktL == true && Keyboard.GetState().IsKeyUp(Keys.L))
+                {
+                    isGedrücktL = false;
+                    EingabeName += "L";
+                }
+
+                if (isGedrücktM == true && Keyboard.GetState().IsKeyUp(Keys.M))
+                {
+                    isGedrücktM = false;
+                    EingabeName += "M";
+                }
+
+                if (isGedrücktN == true && Keyboard.GetState().IsKeyUp(Keys.N))
+                {
+                    isGedrücktN = false;
+                    EingabeName += "N";
+                }
+
+                if (isGedrücktO == true && Keyboard.GetState().IsKeyUp(Keys.O))
+                {
+                    isGedrücktO = false;
+                    EingabeName += "O";
+                }
+
+                if (isGedrücktÖ == true && Keyboard.GetState().IsKeyUp(Keys.OemTilde))
+                {
+                    isGedrücktÖ = false;
+                    EingabeName += "Ö";
+                }
+
+                if (isGedrücktP == true && Keyboard.GetState().IsKeyUp(Keys.P))
+                {
+                    isGedrücktP = false;
+                    EingabeName += "P";
+                }
+
+                if (isGedrücktQ == true && Keyboard.GetState().IsKeyUp(Keys.Q))
+                {
+                    isGedrücktQ = false;
+                    EingabeName += "Q";
+                }
+
+                if (isGedrücktR == true && Keyboard.GetState().IsKeyUp(Keys.R))
+                {
+                    isGedrücktR = false;
+                    EingabeName += "R";
+                }
+
+                if (isGedrücktS == true && Keyboard.GetState().IsKeyUp(Keys.S))
+                {
+                    isGedrücktS = false;
+                    EingabeName += "S";
+                }
+
+                if (isGedrücktT == true && Keyboard.GetState().IsKeyUp(Keys.T))
+                {
+                    isGedrücktT = false;
+                    EingabeName += "T";
+                }
+
+                if (isGedrücktU == true && Keyboard.GetState().IsKeyUp(Keys.U))
+                {
+                    isGedrücktU = false;
+                    EingabeName += "U";
+                }
+
+                if (isGedrücktÜ == true && Keyboard.GetState().IsKeyUp(Keys.OemSemicolon))
+                {
+                    isGedrücktÜ = false;
+                    EingabeName += "Ü";
+                }
+
+                if (isGedrücktV == true && Keyboard.GetState().IsKeyUp(Keys.V))
+                {
+                    isGedrücktV = false;
+                    EingabeName += "V";
+                }
+
+                if (isGedrücktW == true && Keyboard.GetState().IsKeyUp(Keys.W))
+                {
+                    isGedrücktW = false;
+                    EingabeName += "W";
+                }
+
+                if (isGedrücktX == true && Keyboard.GetState().IsKeyUp(Keys.X))
+                {
+                    isGedrücktX = false;
+                    EingabeName += "X";
+                }
+
+                if (isGedrücktY == true && Keyboard.GetState().IsKeyUp(Keys.Y))
+                {
+                    isGedrücktY = false;
+                    EingabeName += "Y";
+                }
+
+                if (isGedrücktZ == true && Keyboard.GetState().IsKeyUp(Keys.Z))
+                {
+                    isGedrücktZ = false;
+                    EingabeName += "Z";
+                }
+            }
+        }
     }
 }
