11007e25a012ea3afea9a84a8859c95e7ace60d9
oOWenderOo <gabriel.moczalla@st.ovgu.de>
2015-09-06 21:52:38 +0200

    - Credits ausgearbeitet und eingefügt

--- GameProject/Crack_Tomb/Crack_Tomb/Crack_Tomb/Menuestruktur/Credits.cs
+++ GameProject/Crack_Tomb/Crack_Tomb/Crack_Tomb/Menuestruktur/Credits.cs
@@ -6,6 +6,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Input;
+using Crack_Tomb.Menuestruktur;
 
 namespace MainMenuCo
 {
@@ -15,49 +16,74 @@ namespace MainMenuCo
         SpriteFont fontText;
         SpriteFont creditsFont;
         Button[] buttons = new Button[1];
+        TriggerButton[] trigger = new TriggerButton[2];
         Texture2D mouse;
         Texture2D background;
+        Texture2D creditsFläche;
         int anzahllevel;
-        int creditsPositionX = 380;
+        int creditsPositionX = 350;
         int creditsPositionY = 50;
-        string[] creditsText = { "Dieses Projekt ist im Rahmen der Veranstaltung",
-                                   "  3D-Gameproject an der Otto-von-Guericke",
-                                   "         Universität entstanden.",
+        int anfang = 0;
+        int ende = 16;
+        string[] creditsText = { "",
                                    "",
                                    "",
-                                   "         Gruppenleitung",
-                                   "",
-                                   "        Gabriel Moczalla",
                                    "",
                                    "",
-                                   "         Programmierung",
                                    "",
-                                   "           Anne Döbler",
-                                   "        Gabriel Moczalla",
-                                   "        Jannick Knechtel",
-                                   "",
-                                   "",
-                                   "       Modelle & Texturen",
-                                   "",
-                                   "           Anne Döbler",
+                                   "Dieses Projekt ist",
+                                   "im Rahmen der Veranstaltung",
+                                   "3D-Gameproject an",
+                                   "der Otto-von-Guericke",
+                                   "Universität entstanden.",
                                    "",
                                    "",
-                                   "             Audio",
                                    "",
-                                   "         Woosh - Mark DiAngelo",
-                                   " The Curtain Rises",
-                                   "    Swords Collide - Sound Explorer",
+                                   "",
+                                   "",
+                                   "",
+                                   "Gruppenleitung",
+                                   "",
+                                   "Gabriel Moczalla",
+                                   "",
+                                   "",
+                                   "Programmierung",
+                                   "",
+                                   "Anne Döbler",
+                                   "Gabriel Moczalla",
+                                   "Jannick Knechtel",
+                                   "",
+                                   "",
+                                   "Modelle & Texturen",
+                                   "",
+                                   "Anne Döbler",
+                                   "",
+                                   "",
+                                   "",
+                                   "",
+                                   "Audio",
+                                   "",
+                                   "Woosh - Mark DiAngelo",
+                                   "The Curtain Rises",
+                                   "Swords Collide",
+                                   "         - Sound Explorer",
                                    "Phantom from Space",
-                                   "         Ossuary 5 - Rest",
-                                   "         Lost Time",
-                                   "   Large Door Slam - SoundBible.com",
-                                   "        Hole Punch - Simon Craggs",
-                                   "        Annodomino - Winnenden Mix"};
+                                   "Ossuary 5 - Rest",
+                                   "Lost Time",
+                                   "Large Door Slam",
+                                   "          - SoundBible.com",
+                                   "Hole Punch - Simon Craggs",
+                                   "Annodomino - Winnenden Mix",
+                                   "",
+                                   ""};
 
         public Credits(int anzahllevel)
         {
             this.anzahllevel = anzahllevel;
             buttons[0] = new Button(new Vector2(60, 370), "MainMenu", "Zurück");
+
+            trigger[0] = new TriggerButton(new Vector2(creditsPositionX, creditsPositionY), "Zurück");
+            trigger[1] = new TriggerButton(new Vector2(creditsPositionX + 300, creditsPositionY), "Weiter");
         }
 
         public override void LoadContent(ContentManager content, GraphicsDeviceManager Graphics) 
@@ -72,8 +98,15 @@ namespace MainMenuCo
                 buttons[i].SetFont(fontButton);
             }
 
+            trigger[0].SetTexture(content.Load<Texture2D>("2DTexturen/Pfeil_links"));
+            trigger[0].SetFont(fontButton);
+
+            trigger[1].SetTexture(content.Load<Texture2D>("2DTexturen/Pfeil_rechts"));
+            trigger[1].SetFont(fontButton);
+
             mouse = content.Load<Texture2D>("2DTexturen/MouseZeiger");
             background = content.Load<Texture2D>("2DTexturen/Hintergrund");
+            creditsFläche = content.Load<Texture2D>("2DTexturen/CreditsFläche");
         }
 
         public override GameState Update(GameTime gameTime)
@@ -83,6 +116,25 @@ namespace MainMenuCo
                 if (buttons[i].isPressed())
                     return buttons[i].GetState(0, anzahllevel);
             }
+
+            if (trigger[0].isPressed())
+            {
+                if (anfang != 0)
+                {
+                    anfang -= 17;
+                    ende -= 17;
+                }
+            }
+
+            if (trigger[1].isPressed())
+            {
+                if (ende != creditsText.Length - 1)
+                {
+                    anfang += 17;
+                    ende += 17;
+                }
+            }
+
             return this;
         }
 
@@ -96,9 +148,16 @@ namespace MainMenuCo
                 buttons[i].Draw(gameTime, Graphics, SpriteBatch);
             }
 
-            for (int i = 0; i < creditsText.Length; i++)
+            SpriteBatch.Draw(creditsFläche, new Vector2(creditsPositionX, creditsPositionY), Color.White);
+
+            for (int i = 0; i < trigger.Length; i++)
             {
-                SpriteBatch.DrawString(creditsFont, creditsText[i], new Vector2(creditsPositionX, creditsPositionY + 15 * i), Color.Black);
+                trigger[i].Draw(gameTime, Graphics, SpriteBatch);
+            }
+
+            for (int i = anfang; i <= ende; i++)
+            {
+                SpriteBatch.DrawString(creditsFont, creditsText[i], new Vector2(creditsPositionX + 100, creditsPositionY + 70 + 15 * (i - anfang)), Color.Black);
             }
 
             SpriteBatch.Draw(mouse, new Vector2(Mouse.GetState().X, Mouse.GetState().Y), Color.White);
